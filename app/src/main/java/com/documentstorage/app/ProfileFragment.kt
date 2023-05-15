2456e5424103f18072e1e18dad16cad7ca940d2a
Robert <soreterobert@gmail.com>
2023-05-15 18:18:34 +0300

    Fixed Stack Activity Bug

--- app/src/main/java/com/documentstorage/app/ProfileFragment.kt
+++ app/src/main/java/com/documentstorage/app/ProfileFragment.kt
@@ -13,16 +13,6 @@ import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
     private lateinit var logoutButton: Button
     private lateinit var firebaseAuth: FirebaseAuth
@@ -49,6 +39,7 @@ class ProfileFragment : Fragment() {
             firebaseAuth.signOut()
             val intent = Intent(context, LoginActivity::class.java)
             startActivity(intent)
+            requireActivity().finish()
         }
     }
 }
